0a9cb23ea9df273265775ef34585961026a8c41b
Chaiwat Shuetrakoonpaiboon <iwatth@gmail.com>
2015-12-21 15:10:10 +0700

    Add MIT license header to each files.

--- padding.go
+++ padding.go
@@ -1,3 +1,11 @@
+// go-padding - Simple padding package for Golang
+
+// Copyright (c) 2015 Chaiwat Shuetrakoonpaiboon. All rights reserved.
+//
+// Use of this source code is governed by a MIT license that can be found in
+// the LICENSE file.
+
+// Package padding provides simple functions for block padding.
 package padding
 
 type BlockPadding interface {
